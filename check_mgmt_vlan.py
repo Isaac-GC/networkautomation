e150de4319f5621201ae3bd808d997b78a7a35c2
Isaac GC <christensenisaac1995@gmail.com>
2020-05-27 11:36:19 -0600

    Network Automation -- update check_mgmt_vlan.py

--- check_mgmt_vlan.py
+++ check_mgmt_vlan.py
@@ -7,6 +7,7 @@ from getpass import getpass
 import yaml
 
 from netmiko.cisco import CiscoIosSSH
+from netmiko.ssh_exception import NetmikoTimeoutException
 from jinja2 import Template
 
 
@@ -23,24 +24,27 @@ def check_mgmt_vlan_by_source(username, password, list_of_ips):
     for ip in list_of_ips:
 
         t_source = ""
+        try:
+            net_connect = CiscoIosSSH(
+                host=ip,
+                username=username,
+                password=password,
+                device_type='cisco_ios'
+            )
 
-        net_connect = CiscoIosSSH(
-            host=ip,
-            username=username,
-            password=password,
-            device_type='cisco_ios'
-        )
+            t_source = net_connect.send_command("show run | inc ip tacacs source")
 
-        t_source = net_connect.send_command("show run | inc ip tacacs source")
+            vlan = re.search(r'[0-9]{3,4}', t_source)
 
-        vlan = re.search(r'[0-9]{3,4}', t_source)
+            device_dict = {
+                "ip": ip,
+                "svlan": vlan.group()
+            }
 
-        device_dict = {
-            "ip": ip,
-            "svlan": vlan.group()
-        }
+            print(template.render(device=device_dict))
 
-        print(template.render(device=device_dict))
+        except NetmikoTimeoutException as e:
+            print(e)
 
 idaho_vtp = []
 
